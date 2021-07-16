c5b14f4091cb31ff667461b27d79b41ea2749d57
mufty <gubriansky@sernato.sk>
2021-07-16 10:14:12 +0200

    doc update

--- scripts/jsdoc-toc.js
+++ scripts/jsdoc-toc.js
@@ -7,7 +7,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"mrp-client.html\">MRP_CLIENT</a>","id":"MRP_CLIENT","children":[]},{"label":"<a href=\"mrp-server.html\">MRP_SERVER</a>","id":"MRP_SERVER","children":[{"label":"<a href=\"mrp-server-employment.html\">employment</a>","id":"MRP_SERVER.employment","children":[]}]},{"label":"<a href=\"utils.html\">utils</a>","id":"utils","children":[]}],
+        data: [{"label":"<a href=\"mrp-client.html\">MRP_CLIENT</a>","id":"MRP_CLIENT","children":[{"label":"<a href=\"mrp-client-employment.html\">employment</a>","id":"MRP_CLIENT.employment","children":[]}]},{"label":"<a href=\"mrp-server.html\">MRP_SERVER</a>","id":"MRP_SERVER","children":[{"label":"<a href=\"mrp-server-employment.html\">employment</a>","id":"MRP_SERVER.employment","children":[]}]},{"label":"<a href=\"utils.html\">utils</a>","id":"utils","children":[]}],
         openedIcon: ' &#x21e3;',
         saveState: false,
         useContextMenu: false
