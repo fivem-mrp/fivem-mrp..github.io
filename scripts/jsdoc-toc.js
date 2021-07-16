23cb6cff2b7de6a6b0478b71a1308530665d0eaf
mufty <gubriansky@sernato.sk>
2021-07-16 19:28:24 +0200

    doc update

--- scripts/jsdoc-toc.js
+++ scripts/jsdoc-toc.js
@@ -6,7 +6,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"MRP_CLIENT.html\">MRP_CLIENT</a>","id":"MRP_CLIENT","children":[{"label":"<a href=\"MRP_CLIENT.business.html\">business</a>","id":"MRP_CLIENT.business","children":[]},{"label":"<a href=\"MRP_CLIENT.employment.html\">employment</a>","id":"MRP_CLIENT.employment","children":[]}]},{"label":"<a href=\"MRP_SERVER.html\">MRP_SERVER</a>","id":"MRP_SERVER","children":[{"label":"<a href=\"MRP_SERVER.business.html\">business</a>","id":"MRP_SERVER.business","children":[]},{"label":"<a href=\"MRP_SERVER.employment.html\">employment</a>","id":"MRP_SERVER.employment","children":[]}]},{"label":"<a href=\"utils.html\">utils</a>","id":"utils","children":[]}],
+        data: [{"label":"<a href=\"global.html\">Globals</a>","id":"global","children":[]},{"label":"<a href=\"MRP_CLIENT.html\">MRP_CLIENT</a>","id":"MRP_CLIENT","children":[{"label":"<a href=\"MRP_CLIENT.business.html\">business</a>","id":"MRP_CLIENT.business","children":[]},{"label":"<a href=\"MRP_CLIENT.employment.html\">employment</a>","id":"MRP_CLIENT.employment","children":[]}]},{"label":"<a href=\"MRP_SERVER.html\">MRP_SERVER</a>","id":"MRP_SERVER","children":[{"label":"<a href=\"MRP_SERVER.business.html\">business</a>","id":"MRP_SERVER.business","children":[]},{"label":"<a href=\"MRP_SERVER.employment.html\">employment</a>","id":"MRP_SERVER.employment","children":[]}]},{"label":"<a href=\"ObjectID.html\">ObjectID</a>","id":"ObjectID","children":[]},{"label":"<a href=\"utils.html\">utils</a>","id":"utils","children":[]}],
         openedIcon: ' &#x21e3;',
         saveState: false,
         useContextMenu: false
