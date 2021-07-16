9c0f20aa829ed9cc5e35e5ca7b293a7a259bc2c8
mufty <gubriansky@sernato.sk>
2021-07-16 12:09:13 +0200

    update doc

--- scripts/jsdoc-toc.js
+++ scripts/jsdoc-toc.js
@@ -1,4 +1,3 @@
-
 (function($) {
     // TODO: make the node ID configurable
     var treeNode = $('#jsdoc-toc-nav');
@@ -7,7 +6,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"mrp-client.html\">MRP_CLIENT</a>","id":"MRP_CLIENT","children":[{"label":"<a href=\"mrp-client-employment.html\">employment</a>","id":"MRP_CLIENT.employment","children":[]}]},{"label":"<a href=\"mrp-server.html\">MRP_SERVER</a>","id":"MRP_SERVER","children":[{"label":"<a href=\"mrp-server-employment.html\">employment</a>","id":"MRP_SERVER.employment","children":[]}]},{"label":"<a href=\"utils.html\">utils</a>","id":"utils","children":[]}],
+        data: [{"label":"<a href=\"MRP_CLIENT.html\">MRP_CLIENT</a>","id":"MRP_CLIENT","children":[{"label":"<a href=\"MRP_CLIENT.employment.html\">employment</a>","id":"MRP_CLIENT.employment","children":[]}]},{"label":"<a href=\"MRP_SERVER.html\">MRP_SERVER</a>","id":"MRP_SERVER","children":[{"label":"<a href=\"MRP_SERVER.employment.html\">employment</a>","id":"MRP_SERVER.employment","children":[]}]},{"label":"<a href=\"utils.html\">utils</a>","id":"utils","children":[]}],
         openedIcon: ' &#x21e3;',
         saveState: false,
         useContextMenu: false
